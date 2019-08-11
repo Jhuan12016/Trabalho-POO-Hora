19acc0311ee071a55235dc10030a86b0e72779ac
Jhuan12016 <49461689+Jhuan12016@users.noreply.github.com>
2019-08-11 15:10:39 -0300

    Create HoraMinutoUtil.java

---
+++ HoraMinutoUtil.java
@@ -0,0 +1,18 @@
+public class HoraMinutoUtil {
+	
+
+	public static void acrescentaMinutos(HoraMinuto h, int minutos) {
+		h.minuto = h.minuto + minutos;
+		
+			while (h.minuto >= 60) {
+				h.minuto = h.minuto - 60;
+				acrescentaHoras(h, 1);		
+			}			
+	}
+	public static void acrescentaHoras (HoraMinuto h, int horas) {
+			h.hora = h.hora + horas;
+		while (h.hora > 23) {				
+				h.hora = h.hora - 24;				
+			}
+	}
+}
